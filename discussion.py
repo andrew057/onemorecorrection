634a24e92bc83f93b3cd0701aae704f59f56b591
andrew057 <52321094+andrew057@users.noreply.github.com>
2019-07-12 16:26:22 +0300

    Update discussion.py

--- discussion.py
+++ discussion.py
@@ -76,7 +76,7 @@ def tostring( string ):
 while True:
     x,y,z= timez()
     if x  %1 ==  0 or y == 0 or z == 0:
-        if date( 2019, mounth(), chislo() ).isoweekday() == 4:
+        if date( 2019, mounth(), chislo() ).isoweekday() == 5:
             result = sqlQuery( 'select * from everyData', 1 )
             i = 0
             string = ''
